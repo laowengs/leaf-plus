6706973ef74876b45ec16ae1e740f3741b19c17b
laowengs <229595412@qq.com>
2024-07-21 12:55:55 +0800

    init

---
+++ src/main/java/io/github/laowengs/leafplus/LeafPlusApplication.java
@@ -0,0 +1,13 @@
+package io.github.laowengs.leafplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/laowengs/leafplus/LeafPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.laowengs.leafplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
