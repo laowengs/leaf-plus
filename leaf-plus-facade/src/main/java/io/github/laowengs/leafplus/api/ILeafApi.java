7f2ebb35beb1b69b965b67481f9e8132f0246568
wengjp <229595412@qq.com>
2024-07-25 18:19:44 +0800

    接口定义

--- leaf-plus-facade/src/main/java/io/github/laowengs/leafplus/api/ILeafApi.java
+++ leaf-plus-facade/src/main/java/io/github/laowengs/leafplus/api/ILeafApi.java
@@ -1,7 +1,29 @@
 package io.github.laowengs.leafplus.api;
 
 public interface ILeafApi {
-    Long get(String var1);
+    /**
+     * 获取最新序列
+     * @param sequenceId
+     * @return
+     */
+    Long get(String sequenceId);
 
-    boolean init();
+    /**
+     * 初始化一个序列 针对号段模式  管理端使用
+     * @return
+     */
+    String init(String businessType);
+
+    /**
+     * 重置一个序列  管理端使用
+     * @param sequenceId
+     * @return
+     */
+    boolean reset(String sequenceId);
+
+    /**
+     * 获取序列信息
+     * @return
+     */
+    String sequenceInfo();
 }
